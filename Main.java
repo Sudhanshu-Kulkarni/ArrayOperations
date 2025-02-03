8e75d68b52c645c25447f9d7ddd3c1b1fcfdc16b
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-02-03 17:56:41 +0530

    Adding comments for better clarity

--- Main.java
+++ Main.java
@@ -2,12 +2,25 @@
 // Name : Sudhanshu Kulkarni
 // PRN : 23070126132
 // Batch : B3
-class Main{
-	public static void main(String args[]){
-		UserInput in = new UserInput();
-		ArrayFunctions functions = new ArrayFunctions();
+class Main {
+    public static void main(String args[]) {
+        UserInput in = new UserInput();
+        ArrayFunctions functions = new ArrayFunctions();
 
-		functions.oddEven(in.arrayInput());
-		functions.min(in.arrayInput());
-	}
+        // Accept user input
+        int[] userArray = in.arrayInput();
+
+        // Separate even and odd numbers
+        functions.oddEven(userArray);
+
+        // Find the minimum neighboring distance index
+        functions.min(userArray);
+
+        // Convert array to ArrayList and vice versa
+        ArrayList<Integer> arrayList = functions.arrayToArrayList(userArray);
+        System.out.println("Converted ArrayList: " + arrayList);
+
+        int[] convertedArray = functions.arrayListToArray(arrayList);
+        System.out.println("Converted back to Array: " + Arrays.toString(convertedArray));
+    }
 }
