24d3e6d24eee99f3a4052e2107ed5f54fa640303
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-02-03 18:01:30 +0530

    Add functionality to convert an ArrayList to an array

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -49,6 +49,14 @@ class ArrayFunctions{
 	    return list;
 	}
 
+	public int[] arrayListToArray(ArrayList<Integer> list) {
+	    int[] array = new int[list.size()];
+	    for (int i = 0; i < list.size(); i++) {
+	        array[i] = list.get(i);
+	    }
+	    return array;
+	}
+
 	
 
 
